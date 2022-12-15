c07eede2386b6ee46c81ff07b43b90d6a3700f18
Kiny <89192734912nik@gmail.com>
2022-12-16 00:22:35 +0300

    start

---
+++ project4-4/project4-4/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  project4-4
+//
+//  Created by nikita on 16.12.2022.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+
+}
+
